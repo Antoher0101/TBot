b397a129f204e18ee185d22b3659991286ac6685
Antoher0101 <firelessball@yandex.ru>
2024-09-21 23:24:05 +0500

    Создание инфраструктуры для интеграции с внешним API #BOT-17

---
+++ modules/raspApi/src/main/java/com/mawus/raspApi/RaspApiApplication.java
@@ -0,0 +1,22 @@
+package com.mawus.raspApi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspApiApplication implements CommandLineRunner {
+    private static Logger log = LoggerFactory.getLogger(RaspApiApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaspApiApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("RaspAPI module joining the thread.");
+        Thread.currentThread().join();
+    }
+}
