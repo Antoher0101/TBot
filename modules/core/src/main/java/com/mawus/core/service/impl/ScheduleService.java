a1152acae5dd2df30ce8d7c2e50ea60faba3685d
Antoher0101 <firelessball@yandex.ru>
2024-09-26 20:27:38 +0500

    Обновление списка городов по cron с использованием scheduler BOT-20

--- modules/core/src/main/java/com/mawus/core/service/impl/ScheduleService.java
+++ modules/core/src/main/java/com/mawus/core/service/impl/ScheduleService.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("core_ScheduleService")
 public class ScheduleService {
     private final Logger log = LoggerFactory.getLogger(ScheduleService.class);
 
@@ -17,7 +17,7 @@ public class ScheduleService {
         this.eventPublisher = eventPublisher;
     }
 
-    @Scheduled(cron = "0 0 4 ? * SAT")
+    @Scheduled(cron = "${core.scheduler.update-city.cron}")
     public void CityUpdateSchedule() {
         log.info("The scheduled task of retrieving cities from the API has begun now.");
         eventPublisher.publishEvent(new CityUpdateScheduleEvent(this));
